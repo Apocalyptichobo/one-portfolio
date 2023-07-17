b42d4883aea19417ff8ad650eef02353f06a9121
Hunter Becker <epicdroid@outlook.com>
2023-07-16 20:05:42 -0400

    --added home screen

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -34,6 +34,7 @@ export const NavBar = () => {
             <h1 className='ml-2 text-5xl font-signature'>Hunter</h1>
         </div>
 
+        {/* For the naviagation bar */}
         <ul className='hidden md:flex'>
             {links.map(({id, link}) => (
             <li key={id} className='px-4 font-medium text-gray-500 capitalize duration-200 cursor-pointer hover:scale-105'>
@@ -41,18 +42,23 @@ export const NavBar = () => {
             </li>
             ))}
         </ul>
-
+        {/* md:hidden causes the hamburger to disappear upon the screen being medium or larger */}
         <div 
         onClick={() => setNav(!nav)} 
-        className='z-10 pr-4 text-gray-500 cursor-pointer'>
+        className='z-10 pr-4 text-gray-500 cursor-pointer md:hidden'> 
             {nav ? <FaTimes size={30} /> : <FaBars size ={30}/>}
         </div>
 
-        <ul className='absolute top-0 left-0 flex flex-col items-center justify-center w-full h-screen text-gray-500 bg-gradient-to-b from-black to-grey-800'>
-            <li className='px-4 py-6 text-4xl capitalize cursor-pointer'>home</li>
-            <li>home</li>
-            <li>home</li>
-        </ul>
+        {/* For the hamburger display */}
+        {nav  && (
+            <ul className='absolute top-0 left-0 flex flex-col items-center justify-center w-full h-screen text-gray-500 bg-gradient-to-b from-black to-gray-800'>          
+            {links.map(({id, link}) => (
+                <li key={id} className='px-4 py-6 text-4xl capitalize cursor-pointer'>
+                    {link}
+                </li>
+            ))}
+            </ul>
+        )}
     </div>
   )
 }
